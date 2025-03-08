4bb2ebd3aef2b5745edc5280d89b2e3dd63805fd
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-03-08 22:27:16 +0000

    Login + Command Handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
+const { token } = require('./config.json');
+
 const client = new Discord.Client({
 	intents: ['Guilds'],
 });
@@ -20,3 +22,23 @@ for (const file of eventFiles) {
 	}
 }
 
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.login(token);
