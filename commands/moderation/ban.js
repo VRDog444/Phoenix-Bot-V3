cc64498cee51f354ad0204ef9a588096c3face3f
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-03-14 21:49:49 +0000

    ban command

---
+++ commands/moderation/ban.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, SlashCommandMentionableOption, SlashCommandStringOption } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ban")
+        .setDescription("Ban a user")
+        .addMentionableOption(new SlashCommandMentionableOption()
+            .setName("user").setDescription("The user to ban").setRequired(true)
+        )
+        .addStringOption(new SlashCommandStringOption()
+            .setName("reason").setDescription("The reason for the ban(The user will be notified)").setRequired(false)
+        ),
+    async execute(interaction) {
+        const user = interaction.options.getMentionable("user");
+        const reason = interaction.options.getString("reason") || "Unspecified";
+        user.kick(reason);
+        user.send(`You have been Banned from \`${interaction.guild.name}\` with reason: \`${reason}\``);
+        interaction.channel.send(`${user} was banned with reason: \`${reason}\``);
+    }
+};
