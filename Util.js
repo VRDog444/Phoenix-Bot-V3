da149c74d8e4ed0f57b9dd13b31f0a825837ad16
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-03-19 18:20:33 +0000

    Permission levels

--- Util.js
+++ Util.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Util = void 0;
+exports.CommandPermissionLevel = exports.Util = void 0;
 class Util {
     static random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
 exports.Util = Util;
+var CommandPermissionLevel;
+(function (CommandPermissionLevel) {
+    CommandPermissionLevel[CommandPermissionLevel["Devs"] = 0] = "Devs";
+    CommandPermissionLevel[CommandPermissionLevel["ServerOwner"] = 1] = "ServerOwner";
+    CommandPermissionLevel[CommandPermissionLevel["ServerAdmins"] = 2] = "ServerAdmins";
+    CommandPermissionLevel[CommandPermissionLevel["ServerMods"] = 3] = "ServerMods";
+    CommandPermissionLevel[CommandPermissionLevel["Anyone"] = 4] = "Anyone";
+})(CommandPermissionLevel || (exports.CommandPermissionLevel = CommandPermissionLevel = {}));
