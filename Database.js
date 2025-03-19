cc795df83ae55a877d36e582102fc04d4d48b826
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-03-19 18:17:45 +0000

    Add Database support for dev support tickets

--- Database.js
+++ Database.js
@@ -39,6 +39,7 @@ const db = new sqlite3.Database("db");
 function GetDB() {
     db.serialize(() => {
         db.run("CREATE TABLE IF NOT EXISTS xp_data (user_id VARCHAR(255), level INT, xp INT, required_xp INT)");
+        db.run("CREATE TABLE IF NOT EXISTS reports (author_id VARCHAR(255), ticket_id VARCHAR(255))");
     });
     return db;
 }
