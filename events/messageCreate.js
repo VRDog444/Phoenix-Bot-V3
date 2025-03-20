10baf48eaaf0da38a32eaad0d9e210c2e54bc574
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-03-20 20:13:40 +0000

    Tweak XP Gain

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -30,14 +30,14 @@ module.exports = {
 
 				db.all("SELECT * FROM xp_data WHERE user_id = ?", message.author.id, (err, rows) => {
 					if (err) console.error(err);
-					const baseXp = Util.random(1, 5);
+					const baseXp = Util.random(1, 7);
 					const realXp = Util.random(1, 5);
 					const user = rows[0];
 					console.log(`Base: ${baseXp} | Real: ${realXp}`);
 					if (baseXp >= realXp) {
 						if ((user.xp += realXp) >= user.required_xp) {
 							const xp = (user.xp + realXp) - user.required_xp;
-							const required_xp = Math.round(user.required_xp *= 1.5);
+							const required_xp = user.level >= 5 ? Math.round(user.required_xp *= 2.5) : Math.round(user.required_xp *= 1.75);
 							db.run("UPDATE xp_data SET xp = ?, level = level + 1, required_xp = ?", { 1: xp, 2: required_xp });
 							const embed = new EmbedBuilder().setColor(0xFF4500);
 						embed.setTitle("Level Up!").setDescription(`${message.member} has advanced to level ${user.level + 1}!`);
