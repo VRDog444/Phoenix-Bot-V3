6013c585c50ef978418ba1b07b85cec667dd16b7
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-04-12 00:28:14 +0100

    Add bubble sort algorithm to Util

--- Util.js
+++ Util.js
@@ -5,6 +5,17 @@ class Util {
     static random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+    static bubbleSort(array) {
+        const n = array.length;
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                }
+            }
+        }
+        return array;
+    }
 }
 exports.Util = Util;
 var CommandPermissionLevel;
--- src/Util.ts
+++ src/Util.ts
@@ -2,6 +2,20 @@ export class Util {
     static random(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    static bubbleSort(array: any[]) {
+        const n = array.length;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                }
+            }
+        }
+
+        return array;
+    }
 }
 
 export enum CommandPermissionLevel {
