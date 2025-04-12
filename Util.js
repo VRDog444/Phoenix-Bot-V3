ccdfdd2847a669a7d52b451afe3e0f264c3094d0
VRDog444 <187321509+VRDog444@users.noreply.github.com>
2025-04-12 17:59:55 +0100

    Fix sorting algorithm bug

--- Util.js
+++ Util.js
@@ -6,15 +6,19 @@ class Util {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
     static bubbleSort(array, levels = true) {
+        if (!array)
+            return;
         const n = array.length;
         if (levels) {
             for (let i = 0; i < n; i++) {
                 for (let j = 0; j < n - i; j++) {
-                    if (array[j]["level"] > array[j]["level"]) {
+                    if (!array[j + 1])
+                        continue;
+                    if (array[j].level > array[j].level) {
                         [array[j], array[j + 1]] = [array[j + 1], array[j]];
                     }
-                    else if (array[j]["level"] === array[j + 1]["level"]) {
-                        if (array[j]["xp"] > array[j + 1]["xp"]) {
+                    else if (array[j].level === array[j + 1].level) {
+                        if (array[j].xp > array[j + 1].xp) {
                             [array[j], array[j + 1]] = [array[j + 1], array[j]];
                         }
                     }
